a230100cc67259d411a19cfe62fe4e0c4c06a6c1
kerong <krameri120@gmail.com>
2025-07-02 10:12:02 +0800

    matplotlib trainning

--- Lab1/main.py
+++ Lab1/main.py
@@ -44,3 +44,41 @@ def generate_XOR_easy():
     # 將輸入與標籤轉換為 NumPy 陣列，labels reshape 成 (21, 1)
     return np.array(inputs), np.array(labels).reshape(21,1)
 
+def plot_data(x, y, title="Data Visualization"):
+    """
+    顯示資料點的散佈圖，依照 label 區分顏色與標記。
+    參數:
+        x: shape = (n, 2) 的輸入資料
+        y: shape = (n, 1) 的標籤資料（值為 0 或 1）
+        title: 圖片標題
+    """
+    # print(f'x={x}')
+    # print(f'y={y}')
+    plt.figure(figsize=(6, 6))
+
+    # 類別 0：紅色圓點
+    plt.scatter(x[y.flatten() == 0][:, 0],
+                x[y.flatten() == 0][:, 1],
+                color='red', label='Class 0')
+
+    # 類別 1：藍色叉叉
+    plt.scatter(x[y.flatten() == 1][:, 0],
+                x[y.flatten() == 1][:, 1],
+                color='blue', marker='x', label='Class 1')
+
+    plt.title(title)
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    plt.legend()
+    plt.grid(True)
+    plt.axis("equal")
+    plt.show()
+
+# # 產生資料
+x, y = generate_linear(n=100)
+# # 畫出資料圖
+plot_data(x, y, title="Linear Dataset")
+#
+# # 產生資料
+# x, y = generate_XOR_easy()
+# plot_data(x, y, title="XOR Dataset")
